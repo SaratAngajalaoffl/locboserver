2c8ce43ef4bbc7002323ebd0bdb574d345891e43
Sarat Angajala <chandrasarat74@gmail.com>
2020-09-11 18:08:26 +0530

    Connected to remote database

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 module.exports = {
 	secretKey: "12345-67890-09876-54321",
-	mongoUrl: "mongodb://localhost:27017/locbo",
+	mongoUrl:
+		"mongodb+srv://armoredvortex:chandrasar@cluster0.6ntom.gcp.mongodb.net/Locbo?retryWrites=true&w=majority",
+	// "mongodb://localhost:27017/locbo",
 	facebook: {
 		clientID: "1142642962773877",
 		clientSecret: "c496ac68e6eaad85980070eadb034f17",
