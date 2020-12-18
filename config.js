14e1db548e0931a6b2002c98151827c5e765cd7d
Angajala Sarat Chandra <60383339+SaratAngajalaoffl@users.noreply.github.com>
2020-12-19 01:28:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-	secretKey: '12345-67890-09876-54321',
+	secretKey: process.env.SECRET || '12345-67890-09876-54321',
 	mongoUrl:
 		'mongodb+srv://armoredvortex:chandrasar@cluster0.6ntom.gcp.mongodb.net/Locbo?retryWrites=true&w=majority',
 	// "mongodb://localhost:27017/locbo",
 	facebook: {
-		clientID: '1142642962773877',
-		clientSecret: 'c496ac68e6eaad85980070eadb034f17',
+		clientID: process.env.CLIENT_ID,
+		clientSecret: process.env.CLIENT_SECRET,
 	},
 };
